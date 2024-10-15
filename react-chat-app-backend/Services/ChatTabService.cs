caf522f3d6ecef0343926e582c51cc11ee6a5093
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-15 19:25:25 +0200

    Update ChatTabService.cs

--- react-chat-app-backend/Services/ChatTabService.cs
+++ react-chat-app-backend/Services/ChatTabService.cs
@@ -1,22 +1,22 @@
-using react_chat_app_backend.Models;
-using react_chat_app_backend.Services.Interfaces;
-
-namespace react_chat_app_backend.Services;
-
-public class ChatTabService : IChatTabService
-{
-    public Task<ChatTab> GetChatTab(string userId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task SaveChatTab(string userId, int unreadMessageCount, bool isHighlighted)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DeleteChatTab(string userId)
-    {
-        throw new NotImplementedException();
-    }
-}
+// using react_chat_app_backend.Models;
+// using react_chat_app_backend.Services.Interfaces;
+//
+// namespace react_chat_app_backend.Services;
+//
+// public class ChatTabService : IChatTabService
+// {
+//     public Task<ChatTab> GetChatTab(string userId)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public Task SaveChatTab(string userId, int unreadMessageCount, bool isHighlighted)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public Task DeleteChatTab(string userId)
+//     {
+//         throw new NotImplementedException();
+//     }
+// }
