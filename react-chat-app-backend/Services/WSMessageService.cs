c22410e50537167f6c49b4c181d43c4ca5a896af
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-16 20:09:32 +0200

    Fix bugs regarding fetching & sending messages
    - Fixed bug when forwarding chat messages: it would only forward a chat message to one websocket connection, so when the user is logged in on multiple devices as example, a chat message sent to him would only arrive at 1 device.
    
    - Fixed bug in FetchHistory: If a user requests history from one websocket connection, the method would send the history to all connected websockets, instead of just responding to the requester.

--- react-chat-app-backend/Services/WSMessageService.cs
+++ react-chat-app-backend/Services/WSMessageService.cs
@@ -12,11 +12,11 @@ public class WSMessageService : IWSMessageService
 {
     private IWSMessageRepository _wsMessageRepository;
     private IWSManager _wsManager;
-    private WSHelpers _wsHelpers;
     private IFriendShipRepository _friendShipRepository;
     private IUserRepository _userRepository;
     private ITokenService _tokenService;
-
+    private WSHelpers _wsHelpers;
+    
     public WSMessageService(
         IWSMessageRepository wsMessageRepository,
         IFriendShipRepository friendShipRepository,
@@ -26,10 +26,10 @@ public class WSMessageService : IWSMessageService
     {
         _wsMessageRepository = wsMessageRepository;
         _wsManager = wsManager;
-        _wsHelpers = new WSHelpers();
         _friendShipRepository = friendShipRepository;
         _userRepository = userRepository;
         _tokenService = tokenService;
+        _wsHelpers = new WSHelpers();
     }
 
     public async Task Handle(WebSocket webSocket, byte[] buffer)
@@ -49,20 +49,17 @@ public class WSMessageService : IWSMessageService
 
     public async Task FetchHistory(WebSocket webSocket, byte[] buffer)
     {
+        var request = _wsHelpers.GetModelData<ChatMessageHistoryRequest>(buffer);
+        var userId1 = request.userId1;
+        var userId2 = request.userId2;
+        var skip = request.skip;
+        var take = request.take;
+        List<ChatMessage> messages;
+        
         try
         {
-            var request = _wsHelpers.GetModelData<ChatMessageHistoryRequest>(buffer);
-            var userId1 = request.userId1;
-            var userId2 = request.userId2;
-            var skip = request.skip;
-            var take = request.take;
-            List<ChatMessage> messages;
-
-            // if (_tokenService.IsTokenValid(userId1, token))
-            // {
-            //     
-            // }
-            //
+            // if (_tokenService.IsTokenValid(userId1, token)){}
+            
             if (skip == 0 && take == 0) {
                 messages = await _wsMessageRepository.GetAllMessages(userId1, userId2);   
             } else {
@@ -71,28 +68,27 @@ public class WSMessageService : IWSMessageService
             
             var messageHistory = new { messages, type = "chatHistory" };
             buffer = _wsHelpers.ToJsonByteArray(messageHistory);
-            await SendMessage(webSocket, buffer);
+            await SendResponseMessage(webSocket, buffer);
         }
         catch (Exception e)
         {
             buffer = _wsHelpers.ToJsonByteArray(new { message = "An unexpected error occurred on the server. Please try again later.", type = "error" });
-            await SendMessage(webSocket, buffer);
+            await SendResponseMessage(webSocket, buffer);
         }
     }
 
     public async Task HandleChatMessage(WebSocket webSocket, byte[] buffer)
     {
+        var message = _wsHelpers.GetModelData<ChatMessage>(buffer);
+        var senderId = message.senderId;
+        var receiverId = message.receiverId;
+        var token = message.token;
+        var user = await _userRepository.GetUser(senderId);
+        message.name = user.name;
+        message.photoURL = user.photoURL;
+        
         try
         {
-            var message = _wsHelpers.GetModelData<ChatMessage>(buffer);
-            var senderId = message.senderId;
-            var receiverId = message.receiverId;
-            var token = message.token;
-            var user = await _userRepository.GetUser(senderId);
-            message.name = user.name;
-            message.photoURL = user.photoURL;
-        
-            
             if (await CheckFriendshipExists(senderId, receiverId)
                 && await CheckFriendshipPending(senderId, receiverId) == false
                  && _tokenService.IsTokenValid(senderId, token))
@@ -104,7 +100,7 @@ public class WSMessageService : IWSMessageService
         catch (Exception e)
         {
             buffer = _wsHelpers.ToJsonByteArray(new { message = "An unexpected error occurred on the server. Please try again later.", type = "error" });
-            await SendMessage(webSocket, buffer);
+            await SendResponseMessage(webSocket, buffer);
         }
     }
     
@@ -115,42 +111,56 @@ public class WSMessageService : IWSMessageService
     
     public async Task Forward(ChatMessage chatMessage)
     {
-        var receiverId = chatMessage.receiverId;
-        var wsClient = _wsManager.Get(receiverId);
-        
-        if (wsClient == null)
-            return;
-        
         var wsMessage = new { chatMessage, type = "chatMessage" };
         var buffer = _wsHelpers.ToJsonByteArray(wsMessage);
         var bufferLength = _wsHelpers.GetLengthWithoutPadding(buffer);
-        
-        var receiverConnection = wsClient.webSocket;
-        await SendMessage(receiverConnection, buffer, bufferLength);
+        await SendMessage(chatMessage.receiverId, buffer, bufferLength);
     }
     
     public async Task SendToUser(string userId, string json)
     {
-        var wsClient = _wsManager.Get(userId);
-
-        if (wsClient == null)
-            return;
-        
-        var userConnection = wsClient.webSocket;
+        // var wsClient = _wsManager.Get(userId);
+        //
+        // if (wsClient == null)
+        //     return;
+        // var userConnection = wsClient.webSocket;
         var buffer = Encoding.UTF8.GetBytes(json);
-        await SendMessage(userConnection, buffer);
+        await SendMessage(userId, buffer);
     }
 
-    public async Task SendMessage(WebSocket webSocket, byte[] buffer, int bufferLength = 0)
+    public async Task SendMessage(string userId, byte[] buffer, int bufferLength = 0)
     {
-        if (webSocket.State != WebSocketState.Open) {
-            return;
-        }
-        
         if (bufferLength == 0) {
             bufferLength = buffer.Length;
         }
+
+        // A user can log into multiple devices at the same time
+        var connections = _wsManager.All()
+            .Where(ws => ws.userId == userId)
+            .Select(ws => ws.webSocket)
+            .ToArray();
         
+        foreach (var conn in connections)
+        {
+            if (conn.State != WebSocketState.Open) {
+                continue;
+            }
+            
+            await conn.SendAsync(
+                new ArraySegment<byte>(buffer, 0, bufferLength),
+                WebSocketMessageType.Text,
+                WebSocketMessageFlags.EndOfMessage,
+                CancellationToken.None
+            );
+        }
+    }
+    
+    public async Task SendResponseMessage(WebSocket webSocket, byte[] buffer, int bufferLength = 0)
+    {
+        if (bufferLength == 0) {
+            bufferLength = buffer.Length;
+        }
+
         await webSocket.SendAsync(
             new ArraySegment<byte>(buffer, 0, bufferLength),
             WebSocketMessageType.Text,
@@ -175,8 +185,6 @@ public class WSMessageService : IWSMessageService
     public async Task<bool> CheckFriendshipPending(string userId1, string userId2)
     {
         var friendship = await _friendShipRepository.GetFriendShip(userId1, userId2);
-        Console.WriteLine(friendship?.isPending);
-        Console.WriteLine(friendship != null && friendship.isPending);
         return friendship != null && friendship.isPending;
     }
     
