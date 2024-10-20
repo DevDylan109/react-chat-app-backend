437a7e1107312bcda4e4e5a979f590a80cbb7d33
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-20 20:14:20 +0200

    Temporarily disable rate limiting for login endpoint

--- react-chat-app-backend/Program.cs
+++ react-chat-app-backend/Program.cs
@@ -29,18 +29,18 @@ builder.Services.AddScoped<IFriendShipService, FriendShipService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
-builder.Services.AddRateLimiter(options =>
-{
-    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-
-    options.AddFixedWindowLimiter("fixed", rateLimitOptions =>
-    {
-        rateLimitOptions.PermitLimit = 10;
-        rateLimitOptions.Window = TimeSpan.FromMinutes(5);
-        rateLimitOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-        rateLimitOptions.QueueLimit = 0;
-    });
-});
+// builder.Services.AddRateLimiter(options =>
+// {
+//     options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+//
+//     options.AddFixedWindowLimiter("fixed", rateLimitOptions =>
+//     {
+//         rateLimitOptions.PermitLimit = 10;
+//         rateLimitOptions.Window = TimeSpan.FromMinutes(5);
+//         rateLimitOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+//         rateLimitOptions.QueueLimit = 0;
+//     });
+// });
 
 builder.Services.AddCors(options =>
 {
@@ -66,7 +66,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseRateLimiter();
+// app.UseRateLimiter();
 
 // Use CORS policy
 app.UseCors("AllowSpecificOrigin");
