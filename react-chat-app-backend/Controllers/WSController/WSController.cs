a497e1c9f4d8a15f0178c61fcf1b809361c22503
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-24 17:03:25 +0200

    Add websocket timeout logic

--- react-chat-app-backend/Controllers/WSController/WSController.cs
+++ react-chat-app-backend/Controllers/WSController/WSController.cs
@@ -11,6 +11,10 @@ public class WSController : ControllerBase
     private IWSMessageService _wsMessageService;
     private ITokenService _tokenService;
     
+    private Timer _timer;
+    private DateTime _lastPingDate = DateTime.Now;
+    private int _pingInterval = 30000;
+    
     public WSController(IWSMessageService wsMessageService, IWSManager wsManager, ITokenService tokenService)
     {
         _wsManager = wsManager;
@@ -29,10 +33,11 @@ public class WSController : ControllerBase
             
             if (!string.IsNullOrEmpty(userId)) {
                 using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                
                 _wsManager.Add(userId, webSocket);
-                await _wsMessageService.BroadcastMessage(userId,new { userId, status = "online", type = "friendStatus" });
                 
+                _timer = new Timer(CheckTimeout, webSocket, _pingInterval, _pingInterval);
+                
+                await _wsMessageService.BroadcastMessage(userId,new { userId, status = "online", type = "friendStatus" });
                 await Listener(webSocket);
             }
         } else {
@@ -59,7 +64,8 @@ public class WSController : ControllerBase
                     break;   
                 }
 
-                // Get the string without trailing bytes, due to buffer being larger than amount received
+                // Get the string without trailing bytes, due to buffer being larger than amount receive,
+                // to make strings compare correctly
                 var str = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
             
                 if (str == "ping") 
@@ -68,8 +74,9 @@ public class WSController : ControllerBase
                     await _wsMessageService.Handle(webSocket, buffer);
             }
         }
-        catch (Exception e) {
-            // Exception occurs when a client loses internet connection, I want to ignore this and continue.
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
         }
         
         await HandleClose(webSocket);
@@ -80,6 +87,8 @@ public class WSController : ControllerBase
     {
         var str = "pong";
         var buffer = Encoding.UTF8.GetBytes(str);
+
+        _lastPingDate = DateTime.Now;
         
         await webSocket.SendAsync(buffer,
             WebSocketMessageType.Text,
@@ -88,12 +97,26 @@ public class WSController : ControllerBase
         );
     }
 
+    // The server will abort the connection when client hasn't sent a ping within the timeframe
+    private void CheckTimeout(object webSocket)
+    {
+        var socket = (WebSocket) webSocket;
+        var timePassed = DateTime.Now - _lastPingDate;
+        
+        if (timePassed.Milliseconds > _pingInterval) {
+            socket.Abort();
+            _timer.DisposeAsync();
+        }
+    }
+
     private async Task HandleClose(WebSocket webSocket)
     {
         var userId = _wsManager.Get(webSocket).userId;
         
         await _wsMessageService.BroadcastMessage(userId, new { userId, status = "offline", type="friendStatus" });
         _wsManager.Remove(webSocket);
+
+        await _timer.DisposeAsync();
     }
 
 }
