a95f939189f14b53d5fa5b6b49e2b1e09e6bc878
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-11-18 01:07:47 +0100

    Update IWSMessageRepository.cs

--- react-chat-app-backend/Repositories/Interfaces/IWSMessageRepository.cs
+++ react-chat-app-backend/Repositories/Interfaces/IWSMessageRepository.cs
@@ -4,7 +4,7 @@ namespace react_chat_app_backend.Repositories.Interfaces;
 
 public interface IWSMessageRepository
 {
-    Task AddMessage(ChatMessage chatMessage);
+    Task StoreMessage(ChatMessage chatMessage);
     Task<List<ChatMessage>> GetAllMessages(string userId1, string userId2);
     Task<List<ChatMessage>> GetMessageSequence(string userId1, string userId2, int skip, int take);
 }
