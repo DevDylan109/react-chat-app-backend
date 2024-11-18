7a8917eb0c2fc47d8fd6ee55ca02e7b4ffa5c384
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-11-18 01:07:56 +0100

    Update WSHelpers.cs

--- react-chat-app-backend/Helpers/WSHelpers.cs
+++ react-chat-app-backend/Helpers/WSHelpers.cs
@@ -7,13 +7,13 @@ namespace react_chat_app_backend.Helpers;
 
 public class WSHelpers
 {
-    public byte[] ToJsonByteArray(object obj)
+    public byte[] ObjectToJsonByteArray(object obj)
     {
         var json = JsonSerializer.Serialize(obj);  
         return Encoding.UTF8.GetBytes(json);
     }
     
-    public T GetModelData<T>(byte[] buffer)
+    public T ByteArrayToObject<T>(byte[] buffer)
     {
         var json = Encoding.UTF8.GetString(buffer).Trim('\0');
         return JsonSerializer.Deserialize<T>(json);
@@ -22,16 +22,13 @@ public class WSHelpers
     public int GetLengthWithoutPadding(byte[] buffer)
     {
         var i = buffer.Length - 1;
-        
-        while (i >= 0 && buffer[i] == 0)
-            i--;
-
+        while (i >= 0 && buffer[i] == 0) i--;
         return i + 1;
     }
     
     public WSMessageType GetMessageType(byte[] buffer)
     {
-        var typeStr = GetModelData<ModelType>(buffer).type;
+        var typeStr = ByteArrayToObject<ModelType>(buffer).type;
         return Enum.Parse<WSMessageType>(typeStr);
     }
 }
