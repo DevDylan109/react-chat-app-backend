7a4f9696c57b3f10e4ed13f1fdd5afba3ed4c523
Dylan <67800304+dylanrmp@users.noreply.github.com>
2024-11-18 01:34:25 +0100

    Update IWSMessageService.cs

--- react-chat-app-backend/Services/Interfaces/IWSMessageService.cs
+++ react-chat-app-backend/Services/Interfaces/IWSMessageService.cs
@@ -5,7 +5,7 @@ namespace react_chat_app_backend.Services.Interfaces;
 
 public interface IWSMessageService
 {
-    Task Handle(WebSocket webSocket, byte[] buffer);
+    Task HandleIncomingMessage(WebSocket webSocket, byte[] buffer);
     Task BroadcastMessage(string userId, object obj);
     Task SendMessage(string userId, object obj);
     Task FetchHistory(WebSocket webSocket, byte[] buffer);
